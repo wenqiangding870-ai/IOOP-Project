4568fe4beebe679df59beed7aee990485a23c121
wenqiangding870-ai <wenqiangding870@gmail.com>
2026-04-05 12:24:11 +0800

    Add files via upload

---
+++ IOOP STUDENT.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IOOP
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+ 
+        {
+            string u = txtUsername.Text;
+            string p = txtPassword.Text;
+
+            if (u == "student" && p == "123")
+            {
+                StudentDashboard s = new StudentDashboard();
+                s.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Login failed");
+            }
+        }
+    }
+    }
+}
